Yggdroot/indentLine

commit af91253d7147ece97caf4c0ee4810f93e82c01b2

Fix indent lines after using ownsyntax
Vim resets the syntax highlighting when the 'ownsyntax' command is used.
This also triggers a new Syntax event, but indentLine did not reapply
its syntax rules because indentLine_enabled is still set.
Now we explicitly unset indentLine_enabled and call Setup again to
ensure everything is working again.

after/plugin/indentLine.vim
@@ -204,10 +204,11 @@ augroup indentLine
                     \ call <SID>Setup() |
                     \ endif
 
-    autocmd BufRead,BufNewFile,ColorScheme,Syntax * call <SID>InitColor()
+    autocmd BufRead,BufNewFile,ColorScheme * call <SID>InitColor()
     autocmd BufUnload * let b:indentLine_enabled = 0 | let b:indentLine_leadingSpaceEnabled = 0
     autocmd SourcePre $VIMRUNTIME/syntax/nosyntax.vim doautocmd indentLine BufUnload
     autocmd FileChangedShellPost * doautocmd indentLine BufUnload | call <SID>Setup()
+    autocmd Syntax * doautocmd indentLine BufUnload | call <SID>Setup()
 augroup END
 
 "{{{1 commands

commit 3fbe5747fe5ac6efaab63f23d88cb9e462edda9d

fix issue #192 this issue is introduced by commit af91253

after/plugin/indentLine.vim
@@ -156,6 +156,10 @@ function! s:Setup()
         call s:InitColor()
     endif
 
+    if ! exists("b:indentLine_setup")
+        let b:indentLine_setup = 1
+    endif
+
     if g:indentLine_enabled
         call s:IndentLinesEnable()
     endif
@@ -208,7 +212,7 @@ augroup indentLine
     autocmd BufUnload * let b:indentLine_enabled = 0 | let b:indentLine_leadingSpaceEnabled = 0
     autocmd SourcePre $VIMRUNTIME/syntax/nosyntax.vim doautocmd indentLine BufUnload
     autocmd FileChangedShellPost * doautocmd indentLine BufUnload | call <SID>Setup()
-    autocmd Syntax * doautocmd indentLine BufUnload | call <SID>Setup()
+    autocmd Syntax * doautocmd indentLine BufUnload | if exists("b:indentLine_setup") | call <SID>Setup() | endif
 augroup END
 
 "{{{1 commands